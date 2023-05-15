57d409a0765954c7836473a4777ec30f0d5dfa1f
nangkyeonglim <nangkyeonglim@users.noreply.github.com>
2023-05-15 07:55:01 +0000

    deploy: d4328642a9edcd589af5d8e232e324d24be15aa6

---
+++ storybook/assets/StepperInput.stories-573b7e30.js
@@ -0,0 +1,10 @@
+import{S as n}from"./StepperInput-a7685fe3.js";import"./jsx-runtime-6eef64cc.js";import"./index-c013ead5.js";import"./_commonjsHelpers-725317a4.js";import"./styled-components.browser.esm-52fdd7b7.js";const c={title:"StepperInput",component:n},e={args:{min:0,max:99,step:1,initialValue:0,getValue:()=>{}}};var r,t,a;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    min: 0,
+    max: 99,
+    step: 1,
+    initialValue: 0,
+    getValue: () => {}
+  }
+}`,...(a=(t=e.parameters)==null?void 0:t.docs)==null?void 0:a.source}}};const u=["Primary"];export{e as Primary,u as __namedExportsOrder,c as default};
+//# sourceMappingURL=StepperInput.stories-573b7e30.js.map
